ddb3abc49545879766057131bda00bf0d03a7801
csuser <csuser@160.10.217.147>
2018-12-04 12:29:15 -0500

    More work done to the viewmodel

---
+++ Time Management/src/edu/westga/cs3211/time_management/viewmodel/TimeManagmentViewModel.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package edu.westga.cs3211.time_management.viewmodel;
+
+
+import edu.westga.cs3211.time_management.model.Calendar;
+import edu.westga.cs3211.time_management.model.Event;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+
+/**
+ * @author Jeremiah Liscum
+ *
+ */
+public class TimeManagmentViewModel {
+	private Calendar calendar;
+	private ListProperty<Event> eventListProperty;
+	/**
+	 * Constructor 
+	 */
+	public TimeManagmentViewModel() {
+		this.calendar = new Calendar();
+		this.eventListProperty = new SimpleListProperty<Event>(FXCollections.observableArrayList(this.calendar.getEvents()));
+		
+	}
+	
+	
+}
